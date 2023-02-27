a422cbc2f9ca587864348c76f55c46b5db2c1eaa
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-02-27 11:52:57 -0400

    completed

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-  $('.open-btn').click(function(){
-    $(".open-btn").parents().next("#myNav").css("width","25%");
-  })
-
-  $('.closebtn').click(function(){
-    $("#myNav").css("width","0%");
-  })
